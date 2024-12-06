eb7142741addcdca851b6671e46cbd8071295a9d
Hongjoon134 <533ubmub@gmail.com>
2024-12-06 14:43:07 +0900

    다시 커밋

---
+++ project_obd/project_obd/frontend/project_obd/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>리액트 페이지에 정상적으로 접근되었습니다!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
